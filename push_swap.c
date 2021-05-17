e1cabf3edbfa4045d22ab7254ebea7e41fcd5f1e
Kevin Rios fuentes <krios-fu@c1r7s2.42madrid.com>
2021-05-17 21:10:46 +0200

    new code

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 15:33:09 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/05/17 18:27:55 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/05/17 21:07:24 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,47 @@ int	push_swap(t_list **stack_a, t_list **stack_b)
 {
 	int		len;
 	int		*sort_array;
+	int		i = 0;
+
+	int 	len_aux;
 
 	len  = ft_lstsize(*stack_a);
-	sort_array = fill_array_int(*stack_a);
-	ft_sort_array(sort_array, ft_lstsize(*stack_a));
+	/*sort_array = fill_array_int(*stack_a);
+	ft_sort_array(sort_array, ft_lstsize(*stack_a));*/
 	while (!check_a(*stack_a, len))
 	{
-		if (len == 3)
+		len_aux = ft_lstsize(*stack_a);
+		i = 0;
+		reverse_rotate(stack_a, 'a');
+		while (i < len_aux)
+		{
+			//print_stacks(*stack_a, *stack_b);
+			
+			if((*stack_a)->content < (*stack_a)->next->content)
+			{
+				swap_stack(*stack_a, 'a');
+				rotate(stack_a, 'a');
+				
+			}
+			else
+			{
+				swap_stack(*stack_a, 'a');
+				push(stack_a, stack_b, 'a');	
+			}
+			
+				i++;
+		}
+		//print_stacks(*stack_a, *stack_b);
+		push_stack_a(stack_a, stack_b);
+		//print_stacks(*stack_a, *stack_b);
+
+
+
+
+
+
+		
+		/*  if (len == 3)
 			push_swap_case_three(stack_a);
 		if (len == 5)
 			push_swap_case_five(stack_a, stack_b);
@@ -61,9 +95,9 @@ int	push_swap(t_list **stack_a, t_list **stack_b)
 			//printf("[[%d]]", get_content( *stack_a, get_min_pos_hold_first(*stack_a)));
 			push_swap_case(stack_a, stack_b, sort_array);
 			push_stack_a(stack_a, stack_b);
-		}
+		}*/
 	}
-	
+	push_stack_a(stack_a, stack_b);
 	return (check_a(*stack_a, len));
 }
 
